47a7de635e10a7cdc231187c567ced5c06193cb4
Ahmed <re.90@live.com>
2017-05-10 22:55:26 +0530

    Uno R3, DHT11 interfacing

---
+++ UnoR3-DHT11-OK/UnoR3-DHT11-OK.ino
@@ -0,0 +1,27 @@
+#include <dht.h>
+
+// Mesh facing front
+// L 2 R
+// 1 --> 5v
+// 2 --> DigitalPin2
+// 3 --> NC
+// 4 --> GND
+
+dht DHT;
+
+#define DHT11_PIN 2
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(2000);
+}
+
