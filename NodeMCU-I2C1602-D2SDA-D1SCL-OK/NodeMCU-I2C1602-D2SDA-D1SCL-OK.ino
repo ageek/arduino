f363529f9e9b2eafca56326e8015e110e558fd6c
Ahmed <re.90@live.com>
2017-05-10 22:49:09 +0530

    NodeMCU I2C1602 LCD interfacing - hello world

---
+++ NodeMCU-I2C1602-D2SDA-D1SCL-OK/NodeMCU-I2C1602-D2SDA-D1SCL-OK.ino
@@ -0,0 +1,50 @@
+#include <LiquidCrystal_I2C.h>
+
+// Construct an LCD object and pass it the 
+// I2C address, width (in characters) and
+// height (in characters). Depending on the
+// Actual device, the IC2 address may change.
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+void setup() {
+
+  // The begin call takes the width and height. This
+  // Should match the number provided to the constructor.
+  lcd.begin(16,2);
+  lcd.init();
+
+  // Turn on the backlight.
+  lcd.backlight();
+
+  // Move the cursor characters to the right and
+  // zero characters down (line 1).
+  lcd.setCursor(12, 0);
+
+  // Print HELLO to the screen, starting at 7,0.
+  lcd.print("->HELLO<-");
+
+  // Move the cursor to the next line and print
+  // WORLD.
+  lcd.setCursor(5, 1);      
+  lcd.print("->WORLD<-");
+}
+
+void loop() {
+  // set the cursor to (0,0):
+  lcd.setCursor(0, 0);
+  // print from 0 to 9:
+  for (int thisChar = 0; thisChar < 10; thisChar++) {
+    lcd.print(thisChar);
+    delay(500);
+  }
+
+  // set the cursor to (16,1):
+  lcd.setCursor(16, 1);
+  // set the display to automatically scroll:
+  lcd.autoscroll();
+  // print from 0 to 9:
+  for (int thisChar = 0; thisChar < 10; thisChar++) {
+    lcd.print(thisChar);
+    delay(500);
+  }
+}
