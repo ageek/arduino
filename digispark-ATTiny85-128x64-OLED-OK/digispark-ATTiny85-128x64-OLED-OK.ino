f1359257ffe2e9b315d71cb0774115d3d142b4b6
Ahmed <re.90@live.com>
2017-05-11 23:49:02 +0530

    digispark ATTiny85 6k Flash - 128x64 0.96' OLED working sample

---
+++ digispark-ATTiny85-128x64-OLED-OK/digispark-ATTiny85-128x64-OLED-OK.ino
@@ -0,0 +1,21 @@
+// http://www.instructables.com/id/ATtiny-Watch-Core/
+// Search for  "Solved! i use this code:"
+#include "SSD1306_minimal.h"
+#include <avr/pgmspace.h>
+
+#define DEG "\xa7" "C"
+
+SSD1306_Mini oled;
+
+void setup(){ 
+oled.init(0x3c);
+oled.clear();
+delay(1000);
+} 
+
+
+void loop(){
+oled.startScreen();
+oled.cursorTo(15,1);
+oled.printString( "Hello Oled_Attiny85 !!!"); 
+}
