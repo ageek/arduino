454d590d9024528faa1e0b1b9d030e9e33ecd2ce
Ahmed <re.90@live.com>
2017-08-26 13:45:58 +0530

    Add temperature to analog clock display - based on Arduino internal temp sensor , and some cosmetic code changes

--- OLED-analog-clock-u8glib-OK/OLED-analog-clock-u8glib-OK.ino
+++ OLED-analog-clock-u8glib-OK/OLED-analog-clock-u8glib-OK.ino
@@ -2,6 +2,7 @@
 // TODO:fix  DS1302  update of analog time
 // BOM: Arduino (pref Uno), DS1302RTC module,0.96inch SSD1306 OLED display
 // NOTE: Below mentioned TMP36 ,RTC1307 are not being used in actual compiled code
+// 
 /******************************************************************************
 OLEd Analog Clock using U8GLIB Library
 visit https://code.google.com/p/u8glib/ for full details of the U8GLIB library and
@@ -33,23 +34,23 @@ Wire TMP36:
 Connect ARef to 3.3v on Arduino
 ******************************************************************************/
 
-// Add libraries
+  // Add libraries
   #include "U8glib.h"
   #include <SPI.h>
   #include <Wire.h>
   #include "RTClib.h"
 
-// Load the virtuabotixRTC library 
+  // Load the virtuabotixRTC library 
   #include "virtuabotixRTC.h"    
 
 
-// setup u8g object
+  // setup u8g object
   U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);  // I2C 
 
          
-// Determines the pins attached to the digital pins of Arduino
-// myRTC (clock, data, rst) 
-virtuabotixRTC myRTC(2, 3, 4);
+   // Determines the pins attached to the digital pins of Arduino
+   // myRTC (clock, data, rst) 
+   virtuabotixRTC myRTC(2, 3, 4);
 
 
 
@@ -61,16 +62,16 @@ virtuabotixRTC myRTC(2, 3, 4);
   int clockCentreX = 64; // used to fix the centre the analog clock
   int clockCentreY = 32; // used to fix the centre the analog clock
   
-//
-//TMP36 Pin Variables
- // boolean useTMP36 = true;  // set this to false if you don not use TMP36
+  //
+  //TMP36 Pin Variables
+  // boolean useTMP36 = true;  // set this to false if you don not use TMP36
   boolean useTMP36 = false;
   #define aref_voltage 3.3 // we tie 3.3V to ARef and measure it with a multimeter!
   int tempPin = 1; //the analog pin the TMP36's Vout (sense) pin is connected to
-//the resolution is 10 mV / degree centigrade with a
-//500 mV offset to allow for negative temperatures
+  //the resolution is 10 mV / degree centigrade with a
+  //500 mV offset to allow for negative temperatures
   int tempReading; // the analog reading from the sensor
-//
+  //
  
 
 void draw(void) {
@@ -104,7 +105,7 @@ void draw(void) {
   if (myRTC.seconds < 10){ thisTime=thisTime + "0";} // add leading zero if required
   thisTime=thisTime + String(myRTC.seconds);
   const char* newTime = (const char*) thisTime.c_str();
-  u8g.drawStr(10,10, newTime);  
+  u8g.drawStr(2,10, newTime);  
 
   // for testing/debugging purposes only
   Serial.println(newTime);
@@ -131,14 +132,14 @@ void draw(void) {
   int x3=(clockCentreX+(sin(angle)*(20)));
   int y3=(clockCentreY-(cos(angle)*(20)));
   u8g.drawLine(clockCentreX,clockCentreY,x3,y3);
-  //
+  
   // display minute hand
   angle = myRTC.minutes * 6 ;
   angle=(angle/57.29577951) ; //Convert degrees to radians  
   x3=(clockCentreX+(sin(angle)*(20-3)));
   y3=(clockCentreY-(cos(angle)*(20-3)));
   u8g.drawLine(clockCentreX,clockCentreY,x3,y3);
-  //
+  
   // display hour hand
   angle = myRTC.hours * 30 + int((myRTC.minutes / 12) * 6 )   ;
   angle=(angle/57.29577951) ; //Convert degrees to radians  
@@ -146,44 +147,50 @@ void draw(void) {
   y3=(clockCentreY-(cos(angle)*(20-11)));
   u8g.drawLine(clockCentreX,clockCentreY,x3,y3);
   
- //==========Temperature 
-// // now add temperature if needed
-// if (useTMP36==true) {
-// //getting the voltage reading from the temperature sensor
-//   tempReading = analogRead(tempPin);   
-// // converting that reading to voltage, for 3.3v arduino use 3.3
-//   float voltage = tempReading * aref_voltage;
-//   voltage /= 1024.0; 
-// // now print out the temperature
-//   int temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
-//   String thisTemp1 = String(temperatureC) + "C";
-//   // printing output as follows used less program storage space
-//   const char* thisTemp = (const char*) thisTemp1.c_str();
-//   u8g.drawStr(100,10,thisTemp); 
-//   // the print command could be used, but uses more memory
-//   //u8g.setPrintPos(100,10);
-//   //u8g.print(thisTemp1);
-// }
-//
-//
+  //==========Temperature 
+  // // now add temperature if needed
+  // if (useTMP36==true) {
+  // //getting the voltage reading from the temperature sensor
+  //   tempReading = analogRead(tempPin);   
+  // // converting that reading to voltage, for 3.3v arduino use 3.3
+  //   float voltage = tempReading * aref_voltage;
+  //   voltage /= 1024.0; 
+  // // now print out the temperature
+  //   int temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+  //   String thisTemp1 = String(temperatureC) + "C";
+  //   // printing output as follows used less program storage space
+  //   const char* thisTemp = (const char*) thisTemp1.c_str();
+  //   u8g.drawStr(100,10,thisTemp); 
+  //   // the print command could be used, but uses more memory
+  //   //u8g.setPrintPos(100,10);
+  //   //u8g.print(thisTemp1);
+  // }
+
+
+  //*********for now lets try print Temp using Arduino internal temp sensor
+   String thisTemp1 = String(GetTemp()) + "C";
+   // printing output as follows used less program storage space
+   const char* thisTemp = (const char*) thisTemp1.c_str();
+   // u8glib API ref: https://github.com/olikraus/u8glib/wiki/userreference 
+   u8g.drawStr(85,10,thisTemp); 
 }
 
 void setup(void) {
   Serial.begin(9600);
   // If you want to set the aref to something other than 5v
-//  analogReference(EXTERNAL);
-//  Wire.begin();
-//  RTC.begin();
-//  if (! RTC.isrunning()) {
-//    Serial.println("RTC is NOT running!");
-//    // following line sets the RTC to the date & time this sketch was compiled
-//    RTC.adjust(DateTime(__DATE__, __TIME__));
-//  }
+  //  analogReference(EXTERNAL);
+  //  Wire.begin();
+  //  RTC.begin();
+  //  if (! RTC.isrunning()) {
+  //    Serial.println("RTC is NOT running!");
+  //    // following line sets the RTC to the date & time this sketch was compiled
+  //    RTC.adjust(DateTime(__DATE__, __TIME__));
+  //  }
 
-   // initial date and time information 
+  // initial date and time information 
   // After the information, comment the line below 
   // (seconds, minutes, hour, day of the week, day of the month, month, year) 
-  myRTC.setDS1302Time(18, 52, 19, 6, 25, 8, 2017);
+  myRTC.setDS1302Time(18, 52, 19, 6, 26, 8, 2017);
 
 }
 
@@ -196,5 +203,38 @@ void loop(void) {
   
   // rebuild the picture after some delay
   delay(1000);
+}
+
+  //********for internal temperature sensor
+  // https://playground.arduino.cc/Main/InternalTemperatureSensor
+double GetTemp(void)
+{
+  unsigned int wADC;
+  double t;
+
+  // The internal temperature has to be used
+  // with the internal reference of 1.1V.
+  // Channel 8 can not be selected with
+  // the analogRead function yet.
+
+  // Set the internal reference and mux.
+  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+  ADCSRA |= _BV(ADEN);  // enable the ADC
+
+  delay(20);            // wait for voltages to become stable.
+
+  ADCSRA |= _BV(ADSC);  // Start the ADC
+
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
+
+  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
+  wADC = ADCW;
+
+  // The offset of 324.31 could be wrong. It is just an indication.
+  t = (wADC - 324.31 ) / 1.22;
+
+  // The returned temperature is in degrees Celsius.
+  return (t);
 }
 
