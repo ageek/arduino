462cb8e2d7cf57af2e768ce964fc34d6b5abba49
Ahmed <re.90@live.com>
2017-09-07 22:43:30 +0530

    Using DS3231 RTC module, DS3231 lib and internal temperature sensor

---
+++ DS3231-using3231lib-internal-temperature-sensor-OK/DS3231-using3231lib-internal-temperature-sensor-OK.ino
@@ -0,0 +1,26 @@
+#include "DS3231.h"
+ 
+DS3231 RTC;
+int tempC;
+int tempF;
+ 
+void setup() {
+  //RTC.begin();
+    // Start the serial interface
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  tempC = RTC.getTemperature();
+  tempF = (tempC * 1.8) + 32.0; // Convert C to F
+ 
+  Serial.print(tempC);
+  Serial.print(" C, ");
+  Serial.print(tempF);
+  Serial.print(" F");
+  
+  Serial.println();
+  
+  delay(1000);
+}
+
