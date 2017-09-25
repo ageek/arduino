60a15a05abb17a3662e727447f34324eb0cccfba
Ahmed <re.90@live.com>
2017-09-25 08:37:35 +0530

    ATTiny85 Nokia 5110LCD DS18b20 temp sensor - instructable

---
+++ unchecked-examples/Attiny85_5110LCD_DS18B20_thermometer/code/code.ino
@@ -0,0 +1,55 @@
+#include <OneWire.h>
+
+#define ONEWIRE_BUSS 4
+ 
+OneWire TemperatureSensor(ONEWIRE_BUSS); 
+
+#include <LCD5110_Basic.h>
+
+//                     __________ SCK (CLK)
+//                    /  ________ MOSI (DIN)
+//                   /  /  ______ DC (register select)
+//                  /  /  /  ____ RST
+//                 /  /  /  /  __ CS (CE)
+//                /  /  /  /  /
+  LCD5110 myGLCD(0, 1, 2, 3, 6); //D6 don't exist - conect CS to GND
+
+extern uint8_t BigNumbers[];
+
+
+void setup(void) {
+  
+   myGLCD.InitLCD();
+   myGLCD.clrScr();
+}
+
+void loop(void) {
+  
+ myGLCD.setFont(BigNumbers);
+ 
+    byte i;
+    byte data[12];
+    int16_t raw;
+    float t;
+ 
+    TemperatureSensor.reset();       // reset one wire buss
+    TemperatureSensor.skip();        // select only device
+    TemperatureSensor.write(0x44);   // start conversion
+ 
+    delay(1000);                     // wait for the conversion
+ 
+    TemperatureSensor.reset();
+    TemperatureSensor.skip();
+    TemperatureSensor.write(0xBE);   // Read Scratchpad
+    
+    for ( i = 0; i < 9; i++) {       // 9 bytes
+      data[i] = TemperatureSensor.read();
+    }
+ 
+    raw = (data[1] << 8) | data[0];
+    t = (float)raw / 16.0;
+ 
+ myGLCD.printNumF(t, 1, CENTER, 14);
+ 
+ 
+}
--- unchecked-examples/Readme.txt
+++ unchecked-examples/Readme.txt
@@ -1,2 +1,5 @@
 This folder contains various examples, code samples collected from around the web. They are yet to be testd, Use at your own risk. Once tested, I'll move them out 
 and into its own project folder/file
+
+attiny85 with nokia 5110-credit goes to the original authors
+https://github.com/thegaragelab/tinytemplate/tree/master/firmware/include
