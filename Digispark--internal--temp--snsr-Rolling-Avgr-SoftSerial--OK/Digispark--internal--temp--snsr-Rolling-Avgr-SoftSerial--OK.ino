6f78e1187ffaaa322f2aad8189890df85aefcf43
Ahmed <re.90@live.com>
2017-09-29 22:57:40 +0530

    Digispark internal temp sensor using TempOffset and Rolling Avg -output to SoftSerial

---
+++ Digispark--internal--temp--snsr-Rolling-Avgr-SoftSerial--OK/Digispark--internal--temp--snsr-Rolling-Avgr-SoftSerial--OK.ino
@@ -0,0 +1,77 @@
+// http://andrey.mikhalchuk.com/2011/06/20/reading-attiny854525-internal-temperature-sensor.html
+#include "InternalTemperatureSensor.h"
+#include "SoftwareSerial.h"
+
+SoftwareSerial TinySerial(3, 4);   // TinySerial(Rx,Tx)
+#define Serial TinySerial   // little hack 
+
+// The 1.0 argument is ignored by default, see the c’tor comments
+InternalTemperatureSensor temperature( 1.0, TEMPERATURE_ADJUSTMENT ); 
+
+void setup() {
+  //initialize serial
+  TinySerial.begin(9600);
+  temperature.init(); // Call init() in setup or every time after you modify ADCSRA or ADMUX
+}
+
+
+void loop() {
+
+  // temperature.print(); // uncomment this to debug sensor output via serial connection
+  int war_sensor_data = temperature.raw();             // This returns barely usable sensor output
+  int temperature_in_celsius = temperature.in_c();   // This returns more usable temperature in degrees Celsius
+  // print to serial
+  //temperature.print();
+
+  Serial.print( " R:" );
+  Serial.println( temperature.raw(), DEC );
+  Serial.print( " L:" );
+  Serial.println( temperature.in_lsb(), DEC );
+  Serial.print( " K:" );
+  Serial.println( temperature.in_k(), DEC );
+  Serial.print( " C:" );
+  Serial.println( temperature.in_c(), DEC );
+  Serial.print( " F:" );
+  Serial.println( temperature.in_f(), DEC );
+  Serial.println( " ######### " );  
+  
+  delay(4000);  //delay of 3s
+  
+}
+
+// from other code samples  - not used,  may be removed to reduce sketch size
+double GetTemp() 
+{
+   // Lets use ATTiny85 internal temperature sensor
+   
+  // https://playground.arduino.cc/Main/InternalTemperatureSensor
+  //double GetTemp(void)
+
+  unsigned int wADC;
+  double t;
+
+  // The internal temperature has to be used
+  // with the internal reference of 1.1V.
+  // Channel 8 can not be selected with
+  // the analogRead function yet.
+
+  // Set the internal reference and mux.
+  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+  ADCSRA |= _BV(ADEN);  // enable the ADC
+
+  delay(20);            // wait for voltages to become stable.
+
+  ADCSRA |= _BV(ADSC);  // Start the ADC
+
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
+
+  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
+  wADC = ADCW;
+
+  // The offset of 324.31 could be wrong. It is just an indication.
+  t = (wADC - 324.31 ) / 1.22;
+
+  // The returned temperature is in degrees Celsius.
+  return (t);
+}
---
+++ Digispark--internal--temp--snsr-Rolling-Avgr-SoftSerial--OK/InternalTemperatureSensor.h
@@ -0,0 +1,49 @@
+#ifndef _INTERNAL_TEMPERATURE_SENSOR_H_
+#define _INTERNAL_TEMPERATURE_SENSOR_H_
+#define TEMPERATURE_SAMPLES 40
+#define TEMPERATURE_ADJUSTMENT -13
+#define EXTREMES_RATIO 5
+#define MAXINT 32767
+#define MININT -32767
+
+
+/*
+ * Based on samples:
+ * – The internal temp sensor has horrible deviation. It’s output varies alot (+/-10 degrees/sec), so requires
+ *   some “smoothing”. I’m removing extreme values and running rolling avg on the rest. This returns ok
+ *   results on 20 samples and more. You can reduce number of samples to reduce response time, but this
+ *   will diminish precision.
+ * – The linearity is ok and approximately 1.0 as the datasheet promised
+ * – The offset need to be calculated for each chip separately and provided in c’tor
+ *
+ * In order to calaculate the offset hook up terminal to pin 2 (Serial TX) 9600 8N1 and use
+ * temperature.print() method call. This will print out temperature readouts. Match it against your
+ * calibrated thermometer attached to the chip. Adjust Tos in c’tor if necessary.
+ * See chapter 17.12 of the ATtiny85 datasheet for a bit more details
+ */
+ 
+class InternalTemperatureSensor {
+  int offset;
+  float coefficient;
+  int readings[TEMPERATURE_SAMPLES];
+  int pos;
+
+  public:
+  InternalTemperatureSensor( float k, int o ) : offset( o ), coefficient( k ), pos( 0 ) {}
+  // Call it every time you need to prepare the chip to read sensor (i.e. in setup)
+  // If you’re using other ADCs besides temperature call it before each temperature reading
+  void init();
+  // Returns the current averaged temperature in LSB
+  int in_lsb();
+  // Returns the current averaged temperature in degrees Celsius
+  int in_c();
+  // Returns the current averaged temperature in degrees Fahrenheit
+  int in_f();
+  // Returns the current averaged temperature in Kelvins
+  int in_k();
+  // Returns the current raw temperature reading from sensor
+  int raw();
+  // Prints the current temperature readings in various formats
+  void print();
+};
+#endif // _INTERNAL_TEMPERATURE_SENSOR_H_
