f239bd19ef2e84d357589012d1fdcc2546b555ac
Ahmed <re.90@live.com>
2017-10-02 21:38:20 +0530

    digispark and ATTiny85 interfacing with Nokia 5110 to display attiny85 internal rolling avg temp

---
+++ Digispark-and-ATtiny85_5110LCD_tiny-temp-sensor-OK/Digispark-and-ATtiny85_5110LCD_tiny-temp-sensor-OK.ino
@@ -0,0 +1,115 @@
+//#include <OneWire.h>
+#include "LCD5110_Basic.h"
+#include "InternalTemperatureSensor.h"
+//#include <SoftwareSerial.h>  //pin2 already blocked by 5110 LCD, also disabled for using same code for digispark kit
+
+
+// refer this for pinout: http://2.bp.blogspot.com/-gSqbotFHKfk/UberFXuAOBI/AAAAAAAAAZo/4IUnODX2KuE/s1600/attiny85_pins.png
+// pin3 --> physical pin 2  , using standard IC pin numbering
+// pin4 --> physical pin 3   
+
+// define Rx, Tx pins
+// Rx of tiny should connect to -->  Tx of USB TTL
+// Tx of tiny -------------------->  Rx of USB TTL
+//SoftwareSerial TinySerial(3, 4); // define pins RX, TX of ATtiny85  as pin3(=physical pin 2), pin4(=physical pin 3) respectively
+// prob conflicts with pi2,3 of attiny being used for 5110 ...
+
+
+//#define ONEWIRE_BUSS 4 
+//OneWire TemperatureSensor(ONEWIRE_BUSS); 
+
+//P0 to P5 of digispark ==> PB0 to PB5 of ATtiny85
+//
+// Nokia 5110 LCD connections to ATtiny85 pins---------DigisparkPins
+// SCK (CLK) --------------- Attiny85 pin 5------------P0
+// MOSI (DIN)--------------- Attiny85 pin 6------------P1
+// DC (register select) ---- Attiny85 pin 7------------P2
+// RST --------------------- Attiny85 pin 2------------P3
+// CS (CE) ----------------- GND-----------------------GND
+
+//                     __________ SCK (CLK)
+//                    /  ________ MOSI (DIN)
+//                   /  /  ______ DC (register select)
+//                  /  /  /  ____ RST
+//                 /  /  /  /  __ CS (CE)
+//                /  /  /  /  /
+  LCD5110 myGLCD(0, 1, 2, 3, 6); //D6 don't exist - conect CS to GND
+
+extern uint8_t BigNumbers[];
+
+// The 1.0 argument is ignored by default, see the c’tor comments
+InternalTemperatureSensor temperature( 1.0, TEMPERATURE_ADJUSTMENT ); 
+
+void setup(void) {
+  
+   myGLCD.InitLCD();
+   myGLCD.clrScr();
+
+   // Open serial communications and let us know we are connected - pin2 blocked by 5110 and we dont have any spare pins to use
+   //TinySerial.begin(9600);
+   //TinySerial.println("Tiny Serial Connected via SoftwareSerial Library");
+
+   temperature.init(); // Call init() in setup or every time after you modify ADCSRA or ADMUX
+}
+
+void loop(void) {
+  
+ myGLCD.setFont(BigNumbers);
+ 
+
+    float t;
+ 
+    
+    myGLCD.printNumF(29.8, 1, CENTER, 14);
+
+    t = (float) temperature.in_c();   // This returns more usable temperature in degrees Celsius
+
+    //myGLCD.printNumF(29.8, 1, CENTER, 14);
+
+    myGLCD.printNumF(t, 1, CENTER, 14);
+
+    // for debugging only - display temp value to soft serial out
+    //TinySerial.print("Temperature= ");
+    //TinySerial.println(t);
+
+    delay(1000);
+}
+
+//Though this method seems to work, using this to display temp somehow prints nothing/junk on 5110 lcd, 
+//hence using the internaltempsensor lib with a rolling avg temp
+
+//float getTemp()       
+//{
+//   // Lets use ATTiny85 internal temperature sensor
+//   
+//  // https://playground.arduino.cc/Main/InternalTemperatureSensor
+//  //double GetTemp(void)
+//
+//  unsigned int wADC;
+//  double t;
+//
+//  // The internal temperature has to be used
+//  // with the internal reference of 1.1V.
+//  // Channel 8 can not be selected with
+//  // the analogRead function yet.
+//
+//  // Set the internal reference and mux.
+//  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+//  ADCSRA |= _BV(ADEN);  // enable the ADC
+//
+//  delay(20);            // wait for voltages to become stable.
+//
+//  ADCSRA |= _BV(ADSC);  // Start the ADC
+//
+//  // Detect end-of-conversion
+//  while (bit_is_set(ADCSRA,ADSC));
+//
+//  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
+//  wADC = ADCW;
+//
+//  // The offset of 324.31 could be wrong. It is just an indication.
+//  t = (wADC - 324.31 ) / 1.22;
+//
+//  // The returned temperature is in degrees Celsius.
+//  return (t);
+//}
---
+++ Digispark-and-ATtiny85_5110LCD_tiny-temp-sensor-OK/InternalTemperatureSensor.cpp
@@ -0,0 +1,158 @@
+#include <avr/io.h>
+#include "InternalTemperatureSensor.h"
+//#include "SoftwareSerial.h"
+
+// for digispark
+//Sketch uses 5156 bytes (85%) of program storage space. Maximum is 6012 bytes.
+//Global variables use 130 bytes of dynamic memory.
+
+//for attiny85
+//Sketch uses 4396 bytes (53%) of program storage space. Maximum is 8192 bytes.
+//Global variables use 124 bytes (24%) of dynamic memory, leaving 388 bytes for local variables. Maximum is 512 bytes.
+
+//You can adjust the rolling average parameters, but overall I do not recommend making TEMPERATURE_SAMPLES 
+//less than 20 and greater than 50. If it’s less than 20 then deviation grows significantly. Making 
+//TEMPERATURE_SAMPLES greater than 50 doesn’t improve precision but wastes memory and slows down calculations.
+//Also EXTREMES_RATIO defines how much of extreme values will be chopped, do not make it less than 2 (that will
+//eliminate all values). Making it more than TEMPERATURE_SAMPLES will effective prevent removing extremes from 
+//the samples.
+
+// refer this for pinout: http://2.bp.blogspot.com/-gSqbotFHKfk/UberFXuAOBI/AAAAAAAAAZo/4IUnODX2KuE/s1600/attiny85_pins.png
+// PB3/pin3 --> physical pin 2  , using standard IC pin numbering of ATTiny85
+// PB4/pin4 --> physical pin 3   
+
+// define Rx, Tx pins
+// Rx of USB TTL should connect to -->  Tx of ATtiny85 PB4 ------> P4 of digispark 
+// Tx of USB TTL should connect to -->  Rx of ATtiny85 PB3 ------> P3 of digispark
+// define pins Rx=PB3, Tx=PB4 of digispark
+//SoftwareSerial TinySerial(3, 4);   // TinySerial(Rx,Tx)
+
+
+
+void InternalTemperatureSensor::init() {
+  //analogReference( INTERNAL1V1 );
+  // ATTiny85 datasheet p140 (17.13.2), p137 (17.12)
+//// // Part A: my code   **************************Not working, something is wrong? 
+// // shows this output:
+// ######### 
+// R:1021
+// L:1023
+// K:1010
+// C:737
+// F:1358
+// #########
+//  // Configure ADMUX
+//  // Reset ADMUX to 0
+//  ADMUX = 0;
+//  // Set ADC4 to 1111 to sense temperature
+//  // Selecting the ADC4 channel by writing the MUX[3:0] bits in ADMUX register to “1111” enables the temperature
+//  // sensor
+//  ADMUX |=  (1 << MUX3) |  (1 << MUX2) | (1 << MUX1) | (1 << MUX0) ;
+//  
+//  // Lets use 1.1v internal voltage reference
+//  //REFS2, REFS1, REFS0
+//  // 0      1       0     //Internal 1.1V Voltage Reference.
+//  ADMUX |= (0 << REFS2); 
+//  ADMUX |= (1 << REFS1); 
+//  ADMUX |= (0 << REFS0);
+//  
+//  // disable auto trigger and disable interrupt
+//  ADMUX &= (0 << ADATE);
+//  ADMUX &= (0 << ADIE);
+//  
+//  // Enable ADC 
+//  ADCSRA |= (1 << ADEN) | ( 1<< ADSC);
+//  
+//  // Set Prescaler to 64, 8MHz /64 = 125000 = 125KHz (50 < X < 200 KHz)
+//  // 64 => 1  1  0
+//  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (0 << ADPS1);
+  
+  // Start conversion
+  //ADCSRA |= (1 << ADSC); 
+  //Wait for conversion to finish 
+  //while (bit_is_set(ADCSRA, ADSC));  // taken care in method raw()
+  
+  
+// // Part B: old code from web  **************************Tested working OK=======
+
+
+
+  // Start with a clean slate
+  ADMUX = 0;
+
+  // Selecting the ADC4 channel by writing the MUX[3:0] bits in ADMUX register to “1111” enables the temperature sensor
+  ADMUX |=  (1 << MUX3) |  (1 << MUX2) | (1 << MUX1) | (1 << MUX0) ;
+  //ADMUX =  _BV(1111);                        // Select temperature sensor, //not working ...use the above line instead
+
+  ADMUX &= ~ _BV( ADLAR );                     // Right-adjust result
+  ADMUX |=  _BV( REFS1 );                      // Set Ref voltage
+  ADMUX &= ~(  _BV( REFS0 ) |  _BV( REFS2 ) ); // to 1.1V
+
+  // Configure ADCSRA
+  ADCSRA &= ~( _BV( ADATE ) | _BV( ADIE ) );   // Disable autotrigger, Disable Interrupt
+  ADCSRA |=  _BV(ADEN);                        // Enable ADC
+  ADCSRA |=  _BV(ADSC);                        // Start first conversion
+  
+  // Seed samples
+  int raw_temp;
+  while( ( ( raw_temp = raw() ) < 0 ) );
+  for( int i = 0; i < TEMPERATURE_SAMPLES; i++ ) {
+    readings[i] = raw_temp;
+  }
+}
+
+
+int InternalTemperatureSensor::in_lsb() {
+  int readings_dup[TEMPERATURE_SAMPLES];
+  int raw_temp;
+  // remember the sample
+  if( ( raw_temp = raw() ) > 0 ) {
+    readings[pos] = raw_temp;
+    pos++;
+    pos %= TEMPERATURE_SAMPLES;
+  }
+  // copy the samples
+  for( int i = 0; i < TEMPERATURE_SAMPLES; i++ ) {
+    readings_dup[i] = readings[i];
+  }
+  // bubble extremes to the ends of the array
+  int extremes_count = TEMPERATURE_SAMPLES / EXTREMES_RATIO;
+  int swap;
+  for( int i = 0; i < extremes_count; ++i ) { // percent of iterations of bubble sort on small N works faster than Q-sort
+  for( int j = 0; j < TEMPERATURE_SAMPLES-1; j++ ) {
+      if( readings_dup[i] > readings_dup[i+1] ) { // could be done with 3 XORs and no swap if you like fancy
+        swap = readings_dup[i];
+        readings_dup[i] = readings_dup[i+1];
+        readings_dup[i+1] = swap;
+      }
+    }
+  }
+  // average the middle of the array
+  int sum_temp = 0;
+  for( int i = extremes_count; i < TEMPERATURE_SAMPLES - extremes_count; i++ ) {
+    sum_temp += readings_dup[i];
+  }
+  return sum_temp / ( TEMPERATURE_SAMPLES - extremes_count * 2 );
+}
+
+int InternalTemperatureSensor::in_c() {
+  return in_k() - 273;
+}
+int InternalTemperatureSensor::in_f() {
+  return in_c() * 9 / 5 + 32;
+}
+int InternalTemperatureSensor::in_k() {
+  return in_lsb() + offset; // for simplicty I'm using k=1, use the next line if you want K!=1.0
+  //return (int)( in_lsb() * coefficient ) + offset;
+}
+int InternalTemperatureSensor::raw() {
+  if( ADCSRA & _BV( ADSC ) ) {
+    return -1;
+  } else {
+    int ret = ADCL | ( ADCH << 8 );   // Get the previous conversion result
+    ADCSRA |= _BV(ADSC);              // Start new conversion
+    return ret;
+  }
+
+}
+
