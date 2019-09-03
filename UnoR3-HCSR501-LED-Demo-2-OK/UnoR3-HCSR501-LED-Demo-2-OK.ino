deb5649707cdcac1d5be505409fdcb70f1a309b6
Ahmed <re.90@live.com>
2019-09-03 23:29:46 +0530

    Simplest HCSR501 demo

---
+++ UnoR3-HCSR501-LED-Demo-2-OK/UnoR3-HCSR501-LED-Demo-2-OK.ino
@@ -0,0 +1,25 @@
+    /////////////////////////////////////////////////////////////////
+   //             Arduino PIR sensor Tutorial           v1.00     //
+  //       Get the latest version of the code here:              //
+ //      http://educ8s.tv/arduino-pir-sensor-tutorial           //
+/////////////////////////////////////////////////////////////////
+
+
+int ledPin = 13;
+int pirPin = 3;
+int val = 0;
+
+void setup() 
+{
+  pinMode (ledPin,OUTPUT);
+  pinMode (pirPin, INPUT);
+}
+void loop () 
+{
+  val = digitalRead(pirPin);
+  digitalWrite(ledPin,val);
+
+if (val == 1)
+  digitalWrite(ledPin,LOW);
+}
+
