4f85d661d430a9e31450ad1f5aff4c617a753f4a
Ahmed <re.90@live.com>
2019-09-22 16:40:44 +0530

    HCSR501 PIR with 5vLowRelay v2- ok -in progress

---
+++ UnoR3-HCSR501-5vLowRelay-v2-OK/UnoR3-HCSR501-5vLowRelay-v2-OK.ino
@@ -0,0 +1,45 @@
+int in1 = 8;
+int sensor = 2;
+int led = 13;
+unsigned long t=0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(in1, OUTPUT);
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+  
+  digitalWrite(in1,HIGH);
+  digitalWrite(led,LOW);
+
+  while(millis()<13000)
+  {
+    digitalWrite(led,HIGH);
+    delay(50);
+    digitalWrite(led,LOW);
+    delay(50);
+  }
+  digitalWrite(led,LOW);
+  
+}
+
+
+void loop() 
+{
+  digitalWrite(in1,HIGH);
+  digitalWrite(led,LOW);
+  if(digitalRead(sensor)==HIGH)
+  {
+   t=millis();
+   while(millis()<(t+5000))
+   {
+   digitalWrite(in1,LOW);
+   digitalWrite(led,HIGH);
+     if((millis()>(t+2300))&&(digitalRead(sensor)==HIGH))
+      {
+       t=millis();
+      }
+   }
+  }
+}
