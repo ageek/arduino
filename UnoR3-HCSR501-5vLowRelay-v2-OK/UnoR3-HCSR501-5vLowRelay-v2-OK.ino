2f3c66140bc169dd2851f1d51aeea37dd2814d2e
Ahmed <re.90@live.com>
2019-09-22 23:14:44 +0530

    PIR sensor v2 code modified a bit

--- UnoR3-HCSR501-5vLowRelay-v2-OK/UnoR3-HCSR501-5vLowRelay-v2-OK.ino
+++ UnoR3-HCSR501-5vLowRelay-v2-OK/UnoR3-HCSR501-5vLowRelay-v2-OK.ino
@@ -1,3 +1,5 @@
+//adapted from web
+//Note: disable all serial outputs for this to work from any power source
 int in1 = 8;
 int sensor = 2;
 int led = 13;
@@ -12,8 +14,10 @@ void setup()
   
   digitalWrite(in1,HIGH);
   digitalWrite(led,LOW);
-
-  while(millis()<13000)
+  Serial.println("Sensor Warming up, pls wait for 30sec ...");
+  // Warm up time for PIR sensor usually 30 to 60 secs is recommended
+  // lets keep it 30s
+  while(millis()<30000)
   {
     digitalWrite(led,HIGH);
     delay(50);
@@ -21,7 +25,7 @@ void setup()
     delay(50);
   }
   digitalWrite(led,LOW);
-  
+  Serial.println("PIR Sensor is now ready !");
 }
 
 
@@ -31,6 +35,9 @@ void loop()
   digitalWrite(led,LOW);
   if(digitalRead(sensor)==HIGH)
   {
+    Serial.print("Motion detected at "); 
+    Serial.print(millis()/1000 );
+    Serial.println("th sec");
    t=millis();
    while(millis()<(t+5000))
    {
